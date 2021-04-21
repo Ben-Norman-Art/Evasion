fdec3455153e5fbad09b66f4e5613cc159a66bbd
unknown <ben.norman34@gmail.com>
2021-04-20 23:57:40 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,12 +13,22 @@ function draw(){
 
   if (state === 'title'){
     title();
+  } else if (state === 'level 1'){
+    level1();
   }
+}
+
+function mousePressed(){
+  state = 'Level 1';
 }
 
 function title(){
   background(220);
   textSize(75);
   stroke(255);
-  text('WELCOME TO MY GAME', 100, 100);
+  text('WELCOME', 100, 100);
+}
+
+function level1(){
+  background(50, 150, 200);
 }
