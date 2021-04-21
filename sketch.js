0868db5a2fa678891f6f56891e6587e0e6b401a7
unknown <ben.norman34@gmail.com>
2021-04-21 15:37:55 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -23,6 +23,10 @@ function draw() {
       level1();
       cnv.mouseClicked(level1MouseClicked);
       break;
+    case 'You Win':
+      youWin();
+      cnv.mouseClicked(youWinMouseClicked);
+      break;
     default:
       break;
   }
@@ -52,4 +56,24 @@ function level1() {
 
 function level1MouseClicked() {
   points++;
+
+  if (points >= 10){
+    state = 'You Win';
+  }
+}
+
+function youWin(){
+  background(255, 50, 80);
+  textSize(75);
+  stroke(255);
+  text('YOU WIN', 100, 100);
+
+
+  textSize(45);
+  text('Click Anywhere To Restart', 100, 300);
+}
+
+function youWinMouseClicked(){
+  state = 'level 1';
+  points = 0; 
 }
