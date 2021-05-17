06efec67853b94d7f216b0e6dadfee6e412097f3
unknown <ben.norman34@gmail.com>
2021-05-17 15:02:19 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,6 +13,9 @@ let meteorsImg;
 function preload() {
   playerImg = loadImage('assets/First_Character_Sprite.png');
   meteorsImg = loadImage('assets/Meteor_Pixelated.png');
+  mountainImg = loadImage('assets/Background_0.png');
+  forestImg = loadImage('assets/Background_1.png');
+  cityImg = loadImage('assets/Background_2.png');
 }
 
 
