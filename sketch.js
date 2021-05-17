57c2afaea6e67c41cc534b6e2b8957adb145601e
unknown <ben.norman34@gmail.com>
2021-05-16 20:35:14 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,7 +7,9 @@ let cnv;
 
 function setup() {
   cnv = createCanvas(600, 600);
-  cnv.mouseClicked(canvasClicked);
+  cnv.mouseClicked(function(){
+
+  });
 }
 
 function draw() {
@@ -23,15 +25,13 @@ function mousePressed(){
   state = 'level 1';
 }
 
-function canvasClicked(){
-
-}
-
 function title() {
   background(100);
   textSize(80);
   stroke(0);
   text('MY GAME', 100, 100);
+
+  text('Click anywhere to start', 100, 300);
 }
 
 function level1(){
