ef69b907f4a3ded09aaad74ec999d0f23af43231
unknown <ben.norman34@gmail.com>
2021-05-16 20:11:10 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,20 +4,26 @@ let state = 'title';
 
 
 
-function setup(){
+function setup() {
   createCanvas(750, 750);
 }
 
-function draw(){
+function draw() {
 
-  if (state === 'title'){
-  title();
+  if (state === 'title') {
+    title();
+  } else if (state === 'level 1') {
+    level1();
   }
 }
 
-function title(){
+function title() {
   background(100);
   textSize(80);
   stroke(0);
   text('MY GAME', 100, 100);
+}
+
+function level1(){
+  background(50, 150, 200);
 }
